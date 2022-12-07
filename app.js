1134c64e1630672e781b565fea40750b9a8f44ed
evandrocgalves <vandred@gmail.com>
2022-12-07 13:13:05 -0300

    Altera para index usar router

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 const express = require('express');
-const fetch = require('node-fetch');
 // const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
+const index = require('./routes/index');
 const login = require('./routes/login');
 const jogos = require('./routes/jogos');
 // const { networkInterfaces } = require('os');
@@ -26,41 +26,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static('public'));
-app.get('/', (req,res, next)=>{    
-    let concurso = '';
-    if(req.query && req.query.concurso)
-        concurso = req.query.concurso;
-    try{
-        fetch(`https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena/${concurso}`,{
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
-        })
-        .then((d)=>{
-            d.json().then(d=>{            
-                if(d.listaDezenas==null)
-                    res.redirect('/');
-                else{
-                    res.render('index',{
-                        title:'Resultados',
-                        concurso:`Concurso ${d.numero}`,
-                        concursoAnterior:d.numeroConcursoAnterior,
-                        concursoPosterior:d.numeroConcursoProximo,
-                        dataApuracao:`Data Apuracao ${d.dataApuracao}`,
-                        numeros:d.listaDezenas
-                    });
-                }
-            });
-        });
-    }
-    catch{
-    }  
-});
+app.use('/', index);
 app.use('/login', login);
 app.use('/jogos', jogos);
-
-app.use((req, res, next) => {
-    res.render('404', {pageTitle: "Page Not Found"});
-});
-
 
 module.exports = app;
