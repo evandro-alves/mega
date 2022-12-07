f0c7533a5b3dcdcc198f4db57328ed7728e1cb85
evandrocgalves <vandred@gmail.com>
2022-12-07 14:39:29 -0300

    teste route

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
-const index = require('./routes/index');
-const login = require('./routes/login');
-const jogos = require('./routes/jogos');
+// const index = require('./routes/index');
+// const login = require('./routes/login');
+// const jogos = require('./routes/jogos');
 // const { networkInterfaces } = require('os');
 
 const app = express();
@@ -25,7 +25,55 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/login', login);
-app.use('/jogos', jogos);
-app.use('/home', index);
+// app.use('/login', login);
+// app.use('/jogos', jogos);
+// app.use('/home', index);
+
+
+router.get('/', (req,res)=>{    
+    let concurso = '';
+    if(req.query && req.query.concurso)
+        concurso = req.query.concurso;
+    try{
+        fetch(`https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena/${concurso}`,{
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then((d)=>{
+            d.json().then(d=>{            
+                if(d.listaDezenas==null)
+                    res.redirect('/');
+                else{
+                    res.render('index',{
+                        title:'Resultados',
+                        concurso:`Concurso ${d.numero}`,
+                        concursoAnterior:d.numeroConcursoAnterior,
+                        concursoPosterior:d.numeroConcursoProximo,
+                        dataApuracao:`Data Apuracao ${d.dataApuracao}`,
+                        numeros:d.listaDezenas
+                    });
+                }
+            });
+        });
+    }
+    catch{
+    }  
+});
+router.get('/jogos',(req,res,next)=>{
+    res.render('jogos');
+});
+router.get('/login',(req,res,next)=>{
+    res.render('login');    
+});
+router.post('/login',(req,res,next)=>{
+    let message = '';
+    try{
+        res.redirect('/jogos');
+    }
+    catch(ex){
+        message = ex;
+    }
+    res.render('login',{message:message});
+});
+
 app.listen(3000);
