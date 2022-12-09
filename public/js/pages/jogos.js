3e74568f3c5a38792721aa3bc0ccffb496b08336
evandrocgalves <vandred@gmail.com>
2022-12-08 21:12:39 -0300

    Implementa pagina de edicao de jogos

--- public/js/pages/jogos.js
+++ public/js/pages/jogos.js
@@ -1,7 +1,22 @@
 const jogos = (()=>{
-    let _public = {};
+    let ajax = {};
+    ajax.AdicionarJogo = ((jogo)=>{
+        return fetch('../jogo/novo',{
+            method:'post',
+            contentType:'application/json',
+            body:JSON.stringify(jogo)
+        });
+    });
+    let evt = {};
+    evt.frmNovoConcursoSubmit=((e)=>{
+        e.preventDefault();
+        ajax.AdicionarJogo({Concurso: e.target.frmNovoConcurso.value})
+            .then()
+    });
+
+    let _public = {};    
     _public.init=()=>{
-        
+        let form = document.querySelector('#frmNovoConcurso').addEventListener('submit',evt.frmNovoConcursoSubmit);
     };
     return _public;
 })();
--- routes/jogos.js
+++ routes/jogos.js
@@ -21,6 +21,44 @@ router.get('/',(req,res,next)=>{
         });
     })
 });
+router.get('/:jogoId',async (req,res,next)=>{
+    if(!req.session.autenticado)
+        res.redirect('/login?return=/jogos');
+    try{
+        await model.get('jogos',req.params.jogoId)
+            .then(async (jogo)=>{
+                if(!jogo || jogo.length == 0){
+                    res.sendStatus(404);
+                }
+                await model.query('select * from jogadores where jogoId=? and usuarioId=?',[jogo.id,req.session.idUsuario])
+                    .then( async (jogadores)=>{
+                        if(!jogadores) jogadores=[];
+                        for(var jogador of jogadores){
+                            await model.query('select * from apostas where jogoid=? and jogadorid=?',[jogo.id, jjogador])
+                                .then((apostas)=>{
+                                    if(!apostas) apostas = [{numeros:[],jogoId:jogo.id,jogadorId:jogador.id}];                                
+                                    jogador.apostas = apostas.map((a)=>{return {jogoId:a.jogoId,jogadorId:a.jogadorId,numeros:a.numeros.split(',')}});
+                                },(err)=>{
+                                    res.sendStatus(500);
+                                });
+                        }
+                        if(jogadores.length==0) jogadores.push({nome:'',jogoId:jogo.id,apostas:[]});
+                        res.render('editarJogo',{
+                            jogo:jogo,
+                            jogadores:jogadores,
+                            autenticado:req.session.autenticado,
+                            usuario:req.session.usuario
+                        });
+                    },(err)=>{
+                        res.sendStatus(500);
+                    });                    
+            },(err)=>{
+                res.sendStatus(500);
+            });
+    }catch(ex){
+        res.redirect('/jogos');
+    }
+});
 router.get('/novo',(req,res)=>{
     if(!req.session.autenticado)
         res.redirect('/login?return=/jogos');
@@ -34,34 +72,44 @@ router.post('/novo',async (req,res)=>{
     if(!req.session.autenticado)
         res.redirect('/login?return=/jogos');
     try{
-        let usuarioId = req.session.idUsuario;    
+        let usuarioId = req.session.idUsuario;
         let jogo = {UsuarioId:usuarioId, Concurso:req.body.concurso, DataCriacao:new Date()};
         await model.add('jogos',jogo)
-        .then(async (jogo)=>{
-                for(var jogador of req.body.jogadores){
-                    await model.add('jogadores',{Nome:jogador.nome,JogoId:jogo.id})
-                    .then(async (j)=>{                  
-                        for(var aposta of jogador.apostas){
-                            await model.add('apostas',{jogoId:jogo.id,jogadorId:j.id,numeros:aposta.numeros.join('-')})
-                            .then((aposta)=>{
-                            },(err)=>{
-                                throw err;
-                            });
-                        }
-                    },(err)=>{
-                        throw err;
-                    })                  
-                }    
-            res.redirect('/jogos');
-        },(err)=>{throw err;});
+        .then((jogo)=>{
+            res.status(201).json({data:[jogo]});
+        },(err)=>{throw 'Não foi possível adicionar o concurso'; });
     }catch(ex){
-        message = ex;
-        res.render('novojogo',{
-            message:message, 
-            autenticado:req.session.autenticado,
-            usuario:req.session.usuario,
-            jogo:req.body
-        });
+        res.status(500).json({err:ex});
     }
+    // try{
+    //     let usuarioId = req.session.idUsuario;    
+    //     let jogo = {UsuarioId:usuarioId, Concurso:req.body.concurso, DataCriacao:new Date()};
+    //     await model.add('jogos',jogo)
+    //     .then(async (jogo)=>{
+    //             for(var jogador of req.body.jogadores){
+    //                 await model.add('jogadores',{Nome:jogador.nome,JogoId:jogo.id})
+    //                 .then(async (j)=>{                  
+    //                     for(var aposta of jogador.apostas){
+    //                         await model.add('apostas',{jogoId:jogo.id,jogadorId:j.id,numeros:aposta.numeros.join('-')})
+    //                         .then((aposta)=>{
+    //                         },(err)=>{
+    //                             throw err;
+    //                         });
+    //                     }
+    //                 },(err)=>{
+    //                     throw err;
+    //                 })                  
+    //             }    
+    //         res.redirect('/jogos');
+    //     },(err)=>{throw err;});
+    // }catch(ex){
+    //     message = ex;
+    //     res.render('novojogo',{
+    //         message:message, 
+    //         autenticado:req.session.autenticado,
+    //         usuario:req.session.usuario,
+    //         jogo:req.body
+    //     });
+    // }
 });
 module.exports = router;
